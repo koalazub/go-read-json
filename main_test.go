19690a3e788c00eb42253225482b55a0743ef335
koalazub <koalaman8@gmail.com>
2021-01-31 00:23:56 +0000

    - tests refactored to include read from tests.json

--- main_test.go
+++ main_test.go
@@ -2,35 +2,32 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 )
 import "testing"
 
-type DummyData struct {
-	data string `json:"[
-	"Star Wars (1977): Great, this film was ★★★★,
-    "Star Wars The Force Awake (2015): A long time ago in a galaxy far far away someone made the best sci-fi film of all time. Then some chap ★★½",
-    "Metropolis: Another movie about a robot. Very strong futuristic look. But also very very old. Hard to understand what was happening becaus ★",
-    "Dr. Strangelove or How I Learned to Stop Worrying and Love the Bomb (1964): Hilarious Kubrick satire ★★★★★",
-    "Plan 9 from outer space (1959): So bad it's bad ½"
-]"`
-}
-
-func TestReadFile(t *testing.T) {
-	openedFile, err := os.Open("../reviews.json")
+func TestOpenFile(t *testing.T) {
+	openedFile, err := os.Open("../tests.json")
 	if openedFile != nil {
 		t.Errorf("Expected read file, but got %v", err)
 	}
 }
 
-func TestGetReview(t *testing.T) {
-	var dummyData DummyData
-	var byteData []byte
-	unmarshalledData := json.Unmarshal(byteData, &dummyData)
+func TestReadFile(t *testing.T) {
+	var content map[string]interface{}
+	openData, err := os.Open("./tests.json")
+	readData, err2 := ioutil.ReadAll(openData)
+	if err2 != nil {
+		t.Errorf("could not read opened file. Check that it is aligned with path and file type")
+	}
+	if err != nil {
+		t.Errorf("Could not read file. Check path for potential failures")
+	}
+	unmarshalledData := json.Unmarshal(readData, &content)
 	if unmarshalledData != nil {
 		t.Errorf("Could not unmarshal JSON data")
 	}
-
 }
 
 func TestReturnLimitedTweetCount(t *testing.T) {
